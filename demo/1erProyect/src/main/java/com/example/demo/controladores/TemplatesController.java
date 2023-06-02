f51e61a5ce02c16bd165c52da10ba124687b1d13
Jimi Joe Torrico Peredo <tano777864@gmail.com>
2023-06-02 17:24:57 -0400

    v25 condicionales, nos falta ciclos

--- demo/1erProyect/src/main/java/com/example/demo/controladores/TemplatesController.java
+++ demo/1erProyect/src/main/java/com/example/demo/controladores/TemplatesController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.*;
 
 @Controller//para hacer uso de los controladores
 @RequestMapping("/templates")//para mapear la url, especificamente templates
@@ -20,4 +21,23 @@ public class TemplatesController {
 		return	"templates/home";
 		
 	}
+	
+	
+	@GetMapping("/atributos")
+	public String atributos(Model model) {
+		Integer num1 = 12;
+		Integer num2 = 13;
+		Integer num3 = 14;
+		Date fecha = new Date();
+		List<String> paises = new ArrayList<String>();
+		//ya creamos las variables de las cuales aremos uso
+		//ahora vamos a crear sus models
+		model.addAttribute("num1", num1);
+		model.addAttribute("num2", num2);
+		model.addAttribute("num3", num3);
+		model.addAttribute("fecha", fecha);
+		model.addAttribute("paises", paises);
+		return "templates/atributos";//llamando al archivo html
+	}
+	
 }
